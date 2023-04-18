6968a937edc033d4e58ca1478084082cf434b242
Caballou <leandro.damian93@gmail.com>
2023-04-18 19:09:46 -0300

    fix framer motion delay

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -75,7 +75,7 @@ const Contact = ({ setSelectedPage }) => {
             initial='hidden'
             whileInView='visible'
             viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.2, duration: 0.5 }}
+            transition={{ delay: 0.4, duration: 0.5 }}
             variants={{
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
@@ -128,7 +128,7 @@ const Contact = ({ setSelectedPage }) => {
               initial='hidden'
               whileInView='visible'
               viewport={{ once: true, amount: 0.5 }}
-              transition={{ delay: 0.4, duration: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
               variants={{
                 hidden: { opacity: 0, x: 50 },
                 visible: { opacity: 1, x: 0 },
